5bfeab112325e6e4e8a47da582955c3d93030b08
Ozasnidev <ozasnidev@gmail.com>
2020-11-03 17:12:31 +0100

    07 - Testing: AddCategory component setCategories call and input reset tested

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -5,6 +5,7 @@ import { AddCategory } from '../../components/GifMemeApp/AddCategory';
 
 describe('AddCategory component test', () => {
     const setCategories = jest.fn();
+    const value = 'Goku';
     let wrapper;
 
     beforeEach( () => {
@@ -17,14 +18,20 @@ describe('AddCategory component test', () => {
     });
 
     test('should to change the input text box', () => {
-        const testedInput = wrapper.find('input');
-        const value = 'Goku';
-
-        testedInput.simulate('change', { target: { value } });
+        wrapper.find('input').simulate('change', { target: { value } });
     });
 
     test('should not to post the info with submit', () => {
         wrapper.find('form').simulate('submit', { preventDefault: () => {} });
         expect(setCategories).not.toHaveBeenCalled();
+    });
+
+    test('should to call setCategories and reset the input', () => {
+        wrapper.find('input').simulate('change', { target: { value } });
+        wrapper.find('form').simulate('submit', { preventDefault: () => {} });
+        expect(setCategories).toHaveBeenCalled();
+        expect(setCategories).toHaveBeenCalledTimes(1);
+        expect(setCategories).toHaveBeenCalledWith(expect.any(Function));
+        expect(wrapper.find('input').prop('value')).toBe('');
     });
 });
