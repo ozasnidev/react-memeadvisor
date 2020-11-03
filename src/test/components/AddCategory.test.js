0e0690d64354619ae04f8cf214fd80fec4993386
ozasnidev <ozasnidev@gmail.com>
2020-11-03 12:49:10 +0100

    05 - Testing: AddCategory input value text changes tested

--- src/test/components/AddCategory.test.js
+++ src/test/components/AddCategory.test.js
@@ -9,4 +9,11 @@ describe('AddCategory component test', () => {
     test('should to show GifList component without issues', () => {
         expect(wrapper).toMatchSnapshot();
     });
+
+    test('should to change the input text box', () => {
+        const testedInput = wrapper.find('input');
+        const value = 'Goku';
+
+        testedInput.simulate('change', { target: { value } });
+    });
 });
